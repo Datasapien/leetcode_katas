fe4672cb08b32da6bbc90111b195b3c8c306c637
Datasapien <bradleyclayton89@gmail.com>
2024-12-16 22:36:17 +0000

    Add docstring

--- src/arrays_and_strings/move_zeroes.py
+++ src/arrays_and_strings/move_zeroes.py
@@ -1,15 +1,19 @@
 # https://algomaster.io/practice/dsa-patterns
 # https://leetcode.com/problems/move-zeroes/description/
 
-# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements. Note that you must do this in-place without making a copy of the array.
+"""
+Given an integer array nums, move all 0's to the end of it while 
+maintaining the relative order of the non-zero elements. 
+Note that you must do this in-place without making a copy of the array.
 
-# Example 1:
-# Input: nums = [0,1,0,3,12]
-# Output: [1,3,12,0,0]
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
 
-# Example 2:
-# Input: nums = [0]
-# Output: [0]
+Example 2:
+Input: nums = [0]
+Output: [0]
+"""
 
 # 3ms, beats 90.22%
 def move_zeroes(nums):
