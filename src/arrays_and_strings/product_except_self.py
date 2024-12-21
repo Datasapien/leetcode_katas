d8a07fc257a264875482fbf9e314a771d7d09e5a
Datasapien <bradleyclayton89@gmail.com>
2024-12-21 16:10:42 +0000

    4 solutions - last one passes for very large lists (but isn't my own :( ...)

--- src/arrays_and_strings/product_except_self.py
+++ src/arrays_and_strings/product_except_self.py
@@ -1,5 +1,8 @@
 # https://algomaster.io/practice/dsa-patterns
 # https://leetcode.com/problems/product-of-array-except-self/description/
+import math
+import functools
+import operator
 
 """
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
@@ -17,10 +20,9 @@ Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 """
 
-# 4 ms, beats 58%
-# 23 MB, beats 28%
+# These solutions work, but times out for larger lists :(
 
-def product_except_self(nums):
+def xproduct_except_self(nums):
 
     new_nums = []
 
@@ -36,3 +38,46 @@ def product_except_self(nums):
         new_nums.append(multiplier)
 
     return new_nums
+
+def xproduct_except_self(nums):
+
+    new_nums = []
+
+    for i in range(len(nums)):
+
+        list_to_multiply = nums[i+1:] + nums[:i]
+
+        new_nums.append(math.prod(list_to_multiply))
+
+    return new_nums
+
+def xproduct_except_self(nums):
+
+    new_nums = []
+
+    for i in range(len(nums)):
+
+        list_to_multiply = nums[i+1:] + nums[:i]
+
+        new_nums.append(functools.reduce(operator.mul, list_to_multiply))
+
+    return new_nums
+
+# From our good friend AI
+def product_except_self(nums):
+    n = len(nums)
+    result = [1] * n
+
+    # Compute prefix products
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+
+    # Compute suffix products and multiply with prefix
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
