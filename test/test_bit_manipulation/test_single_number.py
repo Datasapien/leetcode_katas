14a5cf0aa0e64a964657fe0a9303180d6482cb56
Datasapien <bradleyclayton89@gmail.com>
2024-12-25 16:19:50 +0000

    Basic test suite for single_number

--- test/test_bit_manipulation/test_single_number.py
+++ test/test_bit_manipulation/test_single_number.py
@@ -1,10 +1,40 @@
 import pytest
 from src.bit_manipulation.single_number import single_number
 
-def test_returns_boolean():
+def test_returns_int():
 
-    test_input = ""
+    test_input = [1,2,1,2,3]
     
     result = single_number(test_input)
 
     assert type(result) == int
+
+def test_for_single_num():
+
+    test_input = [1]
+
+    expected_output = 1
+    
+    result = single_number(test_input)
+
+    assert result == expected_output
+
+def test_for_single_three_nums():
+
+    test_input = [2,2,1]
+
+    expected_output = 1
+    
+    result = single_number(test_input)
+
+    assert result == expected_output
+
+def test_for_single_five_nums():
+
+    test_input = [4,1,2,1,2]
+
+    expected_output = 4
+    
+    result = single_number(test_input)
+
+    assert result == expected_output
