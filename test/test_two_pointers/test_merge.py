e05d9f2c82608b2dd0a321a071a25fa3b7378fb8
Datasapien <bradleyclayton89@gmail.com>
2025-01-02 18:00:16 +0000

    Tests for correct return type and example 1

--- test/test_two_pointers/test_merge.py
+++ test/test_two_pointers/test_merge.py
@@ -1,7 +1,7 @@
 import pytest
 from src.two_pointers.merge import merge
 
-def test_():
+def test_returns_list():
 
     test_nums1 = [1,2,3,0,0,0]
     test_m = 3
@@ -13,3 +13,29 @@ def test_():
     result = merge(test_nums1, test_m, test_nums2, test_n)
 
     assert type(result) == list
+
+def test_returns_expected_output_example_1():
+
+    test_nums1 = [1,2,3,0,0,0]
+    test_m = 3
+    test_nums2 = [2,5,6]
+    test_n = 3
+
+    expected_output = [1,2,2,3,5,6]
+    
+    result = merge(test_nums1, test_m, test_nums2, test_n)
+
+    assert result == expected_output
+
+def test_returns_expected_output_example_2():
+
+    test_nums1 = [1,2,3,0,0,0]
+    test_m = 3
+    test_nums2 = [2,5,6]
+    test_n = 3
+
+    expected_output = [1,2,2,3,5,6]
+    
+    result = merge(test_nums1, test_m, test_nums2, test_n)
+
+    assert result == expected_output
