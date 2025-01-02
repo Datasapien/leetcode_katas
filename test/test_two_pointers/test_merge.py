1061a24f35e6f3ca3dd440d5223d26e203548e39
Datasapien <bradleyclayton89@gmail.com>
2025-01-02 18:02:00 +0000

    Tests for example 2 and 3

--- test/test_two_pointers/test_merge.py
+++ test/test_two_pointers/test_merge.py
@@ -29,12 +29,25 @@ def test_returns_expected_output_example_1():
 
 def test_returns_expected_output_example_2():
 
-    test_nums1 = [1,2,3,0,0,0]
-    test_m = 3
-    test_nums2 = [2,5,6]
-    test_n = 3
+    test_nums1 = [1]
+    test_m = 1
+    test_nums2 = []
+    test_n = 0
 
-    expected_output = [1,2,2,3,5,6]
+    expected_output = [1]
+    
+    result = merge(test_nums1, test_m, test_nums2, test_n)
+
+    assert result == expected_output
+
+def test_returns_expected_output_example_3():
+
+    test_nums1 = [0]
+    test_m = 0
+    test_nums2 = [1]
+    test_n = 1
+
+    expected_output = [1]
     
     result = merge(test_nums1, test_m, test_nums2, test_n)
 
